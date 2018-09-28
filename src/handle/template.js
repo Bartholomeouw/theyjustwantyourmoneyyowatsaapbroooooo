10d17070252fdb9feb0da66bfe8a32bd28aa1e1f
fatir1242 <39852500+fatir1242@users.noreply.github.com>
2018-09-28 18:55:11 +0700

    Create template.js

---
+++ src/handle/template.js
@@ -0,0 +1,21 @@
+exports.run = async (client, message, args) => {
+
+}
+
+exports.conf = {
+
+    aliases: [],
+
+    cooldown: ""
+
+}
+
+exports.help = {
+
+    name: "",
+
+    description: "",
+
+    usage: ""
+
+}
