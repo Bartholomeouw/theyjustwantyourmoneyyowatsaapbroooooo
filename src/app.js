842647aab0013710d6fe3171c0be51d3fabbf146
fatir1242 <39852500+fatir1242@users.noreply.github.com>
2018-09-28 18:36:36 +0700

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const codDevClient = require('./handle/meliodasClient');
+
+const client = new meliodasClient({
+  fetchAllMembers: true,
+  disabledEvents: ["TYPING_START", "USER_NOTE_UPDATE"],
+  disableEveryone: true
+
+});
+
+require('./handle/events')(client);
+require('./handle/module')(client);
+
+client.login(process.env.BOT_TOKEN);
